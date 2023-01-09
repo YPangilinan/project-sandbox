1af0d837dd26c9c2aaf911a883a4ba41db53aa48
Yssabel Pangilinan <yssabelpangilinan@yahoo.com>
2023-01-08 21:20:35 -0800

    adding like my photo application

---
+++ src/projects/03-like-my-photo/LikePhotoApp.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import Title from '../components/Title'
+import { AiFillSmile, AiFillHeart, AiOutlineHeart, AiOutlineComment } from "react-icons/ai"
+import DoggyDog from './img/img.jpg';
+
+function LikePhotoApp() {
+  // let like = false;
+  const [like, setLike] = useState(false);
+  const [count, setCount] = useState(0)
+
+  const toggleLike = () => {
+    if(!like){
+      setLike(true);
+      setCount(count + 1);
+    } else {
+      setLike(false);
+      setCount(count - 1);
+    }
+  }
+
+  return (
+    <div className="container text-center">
+      <Title text={"Like Photo App"} />
+      <Title classes={"subtitle"} text={`Likes: ${count}`} />
+      <div className="card card-dark m-auto shadow-sm" style={{width: 300, cursor:"pointer"}}>
+        <div className="card-header fs-xl">
+          <AiFillSmile className="mr-2"/>
+          <small>DoggyDog</small>
+        </div>
+        <img src={DoggyDog} alt="img" style={{height:"fit-content"}} onDoubleClick={toggleLike}/>
+        <div className="card-footer fs-xl d-flex" style={{justifyContent:"space-between"}}>
+        <AiOutlineComment /> {like ? (<AiFillHeart className="text-danger" onClick={toggleLike}/>) : (<AiOutlineHeart onClick={toggleLike} />)}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LikePhotoApp
